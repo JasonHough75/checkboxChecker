db1fe894eb3e45f057a03639ffee1d43eba56c88
JasonHough75 <42379657+JasonHough75@users.noreply.github.com>
2019-01-10 16:13:15 +0000

    Add files via upload

---
+++ statements.ts
@@ -0,0 +1,66 @@
+export default [
+    {
+      id: 1,
+      statement: 'Please quote one penalty charge number (PCN) on all the form(s). This will be 2 letters followed by either 8 numbers or 7 numbers and a letter. We are unable to locate your case without it.'
+      
+    },
+    {
+        id: 2,
+        statement: 'The penalty charge number (PCN) you have quoted has not been registered at the TEC. Please contact the Local Authority concerned regarding the penalty charge'
+        
+      },
+      {
+        id: 3,
+        statement: 'You have not stated your full name and/or address details. Please ensure this is completed before returning your form(s).'
+        
+      },
+      {
+        id: 4,
+        statement: 'The form(s) must be completed in the respondent name unless you are acting on behalf of a respondent company/organisation. If so, you must state this clearly on the form.'
+        },
+    {
+        id: 5,
+        statement: 'You have not indicated the ground you are filing your application on.  '
+        },
+    {
+        id: 6,
+        statement: 'The wording of the grounds selected has been altered.  If none of the grounds applies as expressed on the form, please refer your query to the Local Authority concerned.'
+        },
+    
+    {
+        id: 7,
+        statement: 'For penalty charges for London Boroughs only one ground may be ticked. Please tick only ONE ground.'
+        },
+    {
+        id: 8,
+        statement: 'You have not provided full details regarding the payment as requested on the form. '
+        },
+    {
+        id: 9,
+        statement: 'Please sign and date the form at the bottom in the ‘statement of truth section'
+        },
+    {
+            id: 10,
+            statement: 'Please note that the form(s) must be witnessed as per the instruction notes'
+            },
+        {
+            id: 11,
+            statement: 'Your form has been received outside the timescale allowed. Please complete the attached forms and RETURN THESE TOGETHER.'
+            },
+            {
+                id: 12,
+                statement: 'You have ticked the litigation friend box. If ticking this box, you are required to send either the Court Order or a certificate of suitability (form FP9) with your form(s).'
+                },
+                {
+        id: 13,
+        statement: 'You have not completed both forms so we are unable to process your application. Please complete the attached form and RETURN BOTH FORMS TOGETHER.'
+        }, 
+        {
+            id: 14,
+            statement: 'You have completed the wrong form for this penalty charge number (PCN). Please complete the attached form'
+            },     
+    
+
+    
+        
+];
